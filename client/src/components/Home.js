528685bd33e2da40fa882ff7a810f3852b33b8ac
Jirmer <jan.schirmer@gmail.com>
2025-05-09 14:24:01 +0200

    Add Home.js component to fix build failure

---
+++ client/src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-4">Welcome to Golf App</h1>
+      <p className="text-lg">
+        Track your golf scores, manage courses, and compete with friends!
+      </p>
+    </div>
+  );
+};
+
+export default Home;
