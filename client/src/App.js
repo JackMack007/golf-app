a92a5fb9855537547230ab3c70892bb4a6fe26d8
Jirmer <jan.schirmer@gmail.com>
2025-05-09 14:31:11 +0200

    Fix App.js import for ProfilePage to resolve build failure

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
-import Login from './components/Login';
-import Signup from './components/Signup';
-import Profile from './components/Profile';
+import LoginPage from './components/LoginPage';
+import SignupPage from './components/SignupPage';
+import ProfilePage from './components/ProfilePage'; // Updated to match actual filename
 import AdminUsers from './components/AdminUsers';
 import AdminUserScores from './components/AdminUserScores';
 import AdminCourses from './components/AdminCourses';
+import Courses from './components/Courses';
+import Scores from './components/Scores';
 import { UserProvider } from './context/UserContext';
 import './App.css';
 
@@ -26,9 +28,11 @@ function App() {
           <NavBar setIsAuthenticated={setIsAuthenticated} />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/login" element={isAuthenticated ? <Navigate to="/profile" /> : <Login setIsAuthenticated={setIsAuthenticated} />} />
-            <Route path="/signup" element={isAuthenticated ? <Navigate to="/profile" /> : <Signup setIsAuthenticated={setIsAuthenticated} />} />
-            <Route path="/profile" element={isAuthenticated ? <Profile /> : <Navigate to="/login" />} />
+            <Route path="/courses" element={<Courses />} />
+            <Route path="/scores" element={<Scores />} />
+            <Route path="/login" element={isAuthenticated ? <Navigate to="/profile" /> : <LoginPage setIsAuthenticated={setIsAuthenticated} />} />
+            <Route path="/signup" element={isAuthenticated ? <Navigate to="/profile" /> : <SignupPage setIsAuthenticated={setIsAuthenticated} />} />
+            <Route path="/profile" element={isAuthenticated ? <ProfilePage /> : <Navigate to="/login" />} />
             <Route path="/admin/users" element={<AdminUsers />} />
             <Route path="/admin/users/:userId/scores" element={<AdminUserScores />} />
             <Route path="/admin/courses" element={<AdminCourses />} />
